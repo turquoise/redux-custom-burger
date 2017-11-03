24e1f56bc1de1ed215b7597fd1a0cba38199a9b5
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-03 21:17:53 +0000

    added price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyAux from '../../../hoc/myaux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -16,7 +17,10 @@ const orderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A delicious burder with the following ingredients: </p>
       <ul>{ ingredientSummary }</ul>
-      <p>Continue to checkout?</p>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to checkout</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
     </MyAux>
   );
 };
