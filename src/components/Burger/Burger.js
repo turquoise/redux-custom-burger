ca433797ea0c072defc5a6c2babffb2d6fadc383
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-03 12:42:43 +0000

    added state ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,26 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])]
+      .map( (_, i) => {
+        return <BurgerIngredient key={key + i} type={key} />;
+      });
+    })
+    // flatten the array.
+    .reduce( (prev, next) => {
+      return prev.concat(next);
+    }, []);
+    console.log('transformedIngredients after reduce ', transformedIngredients);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      { transformedIngredients }
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -4,14 +4,19 @@ import Burger from '../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
-  constructor(props) {
-    super(props);
-
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
   }
+
   render() {
     return (
       <MyAux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </MyAux>
     )
