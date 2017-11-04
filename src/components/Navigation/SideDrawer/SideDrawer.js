b8132c89a93a11094543efb52e9d1fc131abbaa7
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-04 16:37:58 +0000

    added hamburger icon

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import MyAux from '../../../hoc/myaux';
 
 const sideDrawer = (props) => {
 
-  return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+  return (
+    <MyAux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </MyAux>
   );
 };
 
