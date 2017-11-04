a6061ebbb861efc9745ad70849947e89ff8d203c
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-04 18:12:49 +0000

    added navigation

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MyAux from '../../../hoc/myaux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map( key => {
-      return (
-        <li key={key}>
-          <span style={ {textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}
-        </li>
-      );
-    });
+class OrderSummary extends Component {
 
-  return (
-    <MyAux>
-      <h3>Your Order</h3>
-      <p>A delicious burder with the following ingredients: </p>
-      <ul>{ ingredientSummary }</ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued} >CONTINUE</Button>
-    </MyAux>
-  );
-};
+  componentWillUpdate() {
+    console.log('ordersummary will update');
+  }
 
-export default orderSummary;
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map( key => {
+        return (
+          <li key={key}>
+            <span style={ {textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}
+          </li>
+        );
+      });
+    return (
+      <MyAux>
+        <h3>Your Order</h3>
+        <p>A delicious burder with the following ingredients: </p>
+        <ul>{ ingredientSummary }</ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued} >CONTINUE</Button>
+      </MyAux>
+    )
+  }
+}
+
+
+
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import MyAux from '../../../hoc/myaux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <MyAux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </MyAux>
+class Modal extends Component {
 
-);
+  shouldComponentUdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log('Modal will update');
+  }
+
+  render () {
+    return (
+      <MyAux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </MyAux>
+    )
+  }
+}
+
+
+export default Modal;
