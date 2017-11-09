f834182169e97531bbac6ebb9ed2a71c808cd143
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-09 11:09:10 +0000

    added orders from firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,62 +2,66 @@ import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
-
-  state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postCode: ''
-    },
-    loading: false
-  }
-
-  orderHandler = (event) => {
-    event.preventDefault();
-    console.log('contactdata ', this.props.ingredients);
-    this.setState( { loading: true } );
-    const order = {
-        ingredients: this.props.ingredients,
-        price: this.props.price,
-        customer: {
-            name: 'Max Schwarzmüller',
-            address: {
-                street: 'Teststreet 1',
-                zipCode: '41351',
-                country: 'Germany'
-            },
-            email: 'test@test.com'
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
         },
-        deliveryMethod: 'fastest'
+        loading: false
     }
-    axios.post( '/orders.json', order )
-        .then( response => {
-            this.setState( { loading: false} );
-        } )
-        .catch( error => {
-            this.setState( { loading: false } );
-        } );
-  }
 
-  render() {
-    return (
-      <div className={classes.ContactData}>
-        <h4>Enter your contact details</h4>
-        <form>
-          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-          <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-          <input className={classes.Input} type="text" name="street" placeholder="Street" />
-          <input className={classes.Input} type="text" name="postcode" placeholder="Postcode" />
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+    orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Max Schwarzmüller',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
+                this.props.history.push('/');
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
+    }
 
-        </form>
-      </div>
-    );
-  }
-
+    render () {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
+        }
+        return (
+            <div className={classes.ContactData}>
+                <h4>Enter your Contact Data</h4>
+                {form}
+            </div>
+        );
+    }
 }
 
 export default ContactData;
