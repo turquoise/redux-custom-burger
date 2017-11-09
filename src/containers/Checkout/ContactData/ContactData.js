850156583dd62aafdf023c996aa0d3a1b844514c
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-09 00:24:12 +0000

    added axios in contactdata

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
 
@@ -10,8 +11,37 @@ class ContactData extends Component {
     address: {
       street: '',
       postCode: ''
-    }
+    },
+    loading: false
   }
+
+  orderHandler = (event) => {
+    event.preventDefault();
+    console.log('contactdata ', this.props.ingredients);
+    this.setState( { loading: true } );
+    const order = {
+        ingredients: this.props.ingredients,
+        price: this.props.price,
+        customer: {
+            name: 'Max Schwarzmüller',
+            address: {
+                street: 'Teststreet 1',
+                zipCode: '41351',
+                country: 'Germany'
+            },
+            email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
+    }
+    axios.post( '/orders.json', order )
+        .then( response => {
+            this.setState( { loading: false} );
+        } )
+        .catch( error => {
+            this.setState( { loading: false } );
+        } );
+  }
+
   render() {
     return (
       <div className={classes.ContactData}>
@@ -21,7 +51,7 @@ class ContactData extends Component {
           <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="postcode" placeholder="Postcode" />
-          <Button btnType="Success" >ORDER</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
 
         </form>
       </div>
